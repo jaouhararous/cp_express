05bbd8b2815d4bd2415ef02bc6bc65960b432220
jaouhararous <jaouhararous@gmail.com>
2023-05-25 09:00:39 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+// const express = require('express');
+// const moment = require('moment');
+// const app = express();
+// const port = 3000;
+
+// // Custom middleware to verify working hours
+// const verifyWorkingHours = (req, res, next) => {
+//   const currentTime = moment();
+//   const dayOfWeek = currentTime.day();
+//   const hour = currentTime.hour();
+
+//   if (dayOfWeek >= 1 && dayOfWeek <= 5 && hour >= 9 && hour < 17) {
+//     next();
+//   } else {
+//     res.send('comeback during working hours (Monday to Friday, from 9 to 17).');
+//   }
+// };
+
+// // Middleware
+// app.use(express.static('public'));
+
+// // Set Pug as the template engine
+// app.set('views', './views');
+// app.set('view engine', 'pug');
+
+// // Routes
+// app.get('/', verifyWorkingHours, (req, res) => {
+//   res.render('home');
+// });
+
+// app.get('/services', verifyWorkingHours, (req, res) => {
+//   res.render('services');
+// });
+
+// app.get('/contact', verifyWorkingHours, (req, res) => {
+//   res.render('contact');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.static('public'));
+
+// Set Pug as the template engine
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/services', (req, res) => {
+  res.render('services');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
